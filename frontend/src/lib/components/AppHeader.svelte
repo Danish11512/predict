2dacbbffe08a43ef0f6d66b3a62378a44e78c653
Danish11512 <danish.faruqi1@gmail.com>
2026-04-02 21:09:55 -0400

    abstract out the svgs

--- frontend/src/lib/components/AppHeader.svelte
+++ frontend/src/lib/components/AppHeader.svelte
@@ -3,6 +3,10 @@
 	import { TopNavTab } from '$lib/interfaces/topNavTab'
 	import { topNavTab } from '$lib/stores/topNavTabStore'
 	import { APP_HEADER_DOM_ID, APP_HEADER_MEDIA_QUERY } from '$lib/constants/appHeader'
+	import {
+		APP_HEADER_THEME_TOGGLE_DARK_MODE_PATH,
+		APP_HEADER_THEME_TOGGLE_LIGHT_MODE_PATH
+	} from '$lib/constants/appHeaderIcons'
 	import { APP_HEADER_TABS } from '$lib/constants/appHeaderTabs'
 	import { isEventTargetInsideElementId } from '$lib/utils/dom'
 	import { listenMediaQuery } from '$lib/utils/mediaQuery'
@@ -112,17 +116,11 @@
 {#snippet themeToggleIcon()}
 	{#if isDarkMode}
 		<svg class="app-header__icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-			<path
-				fill="currentColor"
-				d="M21 13.6A8.4 8.4 0 0 1 10.4 3a.6.6 0 0 0-.8-.7A9.6 9.6 0 1 0 21.7 14.4a.6.6 0 0 0-.7-.8Z"
-			/>
+			<path fill="currentColor" d={APP_HEADER_THEME_TOGGLE_DARK_MODE_PATH} />
 		</svg>
 	{:else}
 		<svg class="app-header__icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-			<path
-				fill="currentColor"
-				d="M12 18.2a6.2 6.2 0 1 1 0-12.4a6.2 6.2 0 0 1 0 12.4ZM12 1.4a.8.8 0 0 1 .8.8v2a.8.8 0 0 1-1.6 0v-2a.8.8 0 0 1 .8-.8Zm0 18.4a.8.8 0 0 1 .8.8v2a.8.8 0 0 1-1.6 0v-2a.8.8 0 0 1 .8-.8ZM4.2 3.9a.8.8 0 0 1 1.1 0l1.4 1.4a.8.8 0 1 1-1.1 1.1L4.2 5a.8.8 0 0 1 0-1.1Zm13.2 13.2a.8.8 0 0 1 1.1 0l1.4 1.4a.8.8 0 1 1-1.1 1.1l-1.4-1.4a.8.8 0 0 1 0-1.1ZM1.4 12a.8.8 0 0 1 .8-.8h2a.8.8 0 0 1 0 1.6h-2a.8.8 0 0 1-.8-.8Zm18.4 0a.8.8 0 0 1 .8-.8h2a.8.8 0 0 1 0 1.6h-2a.8.8 0 0 1-.8-.8ZM3.9 19.8a.8.8 0 0 1 0-1.1l1.4-1.4a.8.8 0 1 1 1.1 1.1l-1.4 1.4a.8.8 0 0 1-1.1 0Zm13.2-13.2a.8.8 0 0 1 0-1.1l1.4-1.4a.8.8 0 1 1 1.1 1.1l-1.4 1.4a.8.8 0 0 1-1.1 0Z"
-			/>
+			<path fill="currentColor" d={APP_HEADER_THEME_TOGGLE_LIGHT_MODE_PATH} />
 		</svg>
 	{/if}
 {/snippet}
